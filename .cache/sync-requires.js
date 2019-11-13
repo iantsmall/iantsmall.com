80d2d16108fe7be2ed47efbef6a6fb12902132d5
Ian T. Small <hello@iantsmall.com>
2019-11-13 04:53:07 +0000

    Built latest version of page

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/ubuntu/environment/iantsmall.com/.cache/dev-404-page.js"))),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/home/ubuntu/environment/iantsmall.com/node_modules/gatsby-plugin-offline/app-shell.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/ubuntu/environment/iantsmall.com/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/ubuntu/environment/iantsmall.com/src/pages/index.js")))
 }
